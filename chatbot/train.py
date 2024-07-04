561dbeee403f9359b8aa80f7b655583ca6ead57b
Demiana Ayman <133611525+Demi-ayman@users.noreply.github.com>
2024-07-04 13:57:04 +0300

    Update train.py

--- chatbot/train.py
+++ chatbot/train.py
@@ -1,62 +1,72 @@
-import json
 import numpy as np
 import random
+import json
 
-import torch 
+import torch
 import torch.nn as nn
-from torch.utils.data import Dataset ,DataLoader 
+from torch.utils.data import Dataset, DataLoader
 
-from nlp_utils import tokenize, stem , bag_of_words
+from nlp_utils import bag_of_words, tokenize, stem
 from model import NeuralNet
 
-
-
-with open('intents.json' ,'r') as f:
+with open('C:\MyOwnProject_AI\chatbot\intents.json', 'r') as f:
     intents = json.load(f)
-
-#print(intents)
 
 all_words = []
 tags = []
 xy = []
+# loop through each sentence in our intents patterns
 for intent in intents['intents']:
     tag = intent['tag']
+    # add to tag list
     tags.append(tag)
     for pattern in intent['patterns']:
+        # tokenize each word in the sentence
         w = tokenize(pattern)
+        # add to our words list
         all_words.extend(w)
-        xy.append((w , tag))
+        # add to xy pair
+        xy.append((w, tag))
 
-ignore_word = [',','?','!','.']
-all_words = [stem(w) for w in all_words if w not in ignore_word]
+# stem and lower each word
+ignore_words = ['?', '.', '!']
+all_words = [stem(w) for w in all_words if w not in ignore_words]
+# remove duplicates and sort
 all_words = sorted(set(all_words))
 tags = sorted(set(tags))
 
-x_train = []
-y_train = []
+print(len(xy), "patterns")
+print(len(tags), "tags:", tags)
+print(len(all_words), "unique stemmed words:", all_words)
 
+# create training data
+X_train = []
+y_train = []
 for (pattern_sentence, tag) in xy:
-    bag = bag_of_words(pattern_sentence,all_words)
-    x_train.append(bag)
-    
-    label =tags.index(tag)
-    y_train.append(label) # CrossEntropyLoss
-x_train = np.array(x_train)
+    # X: bag of words for each pattern_sentence
+    bag = bag_of_words(pattern_sentence, all_words)
+    X_train.append(bag)
+    # y: PyTorch CrossEntropyLoss needs only class labels, not one-hot
+    label = tags.index(tag)
+    y_train.append(label)
+
+X_train = np.array(X_train)
 y_train = np.array(y_train)
 
-# hyperparameter
+# Hyper-parameters 
+num_epochs = 1000
 batch_size = 8
-input_size = len(x_train[0])
+learning_rate = 0.001
+input_size = len(X_train[0])
 hidden_size = 8
 output_size = len(tags)
-learning_rate = 0.001
-num_epochs = 1000
+print(input_size, output_size)
 
 class ChatDataset(Dataset):
 
     def __init__(self):
-        self.n_samples = len(x_train)
-        self.x_data = x_train
+        self.n_samples = len(X_train)
+        self.x_data = X_train
         self.y_data = y_train
 
     # support indexing such that dataset[i] can be used to get i-th sample
@@ -69,17 +79,19 @@ class ChatDataset(Dataset):
 
 dataset = ChatDataset()
 train_loader = DataLoader(dataset=dataset,
-                        batch_size=batch_size,
-                        shuffle=True,
-                        num_workers=0)
+                          batch_size=batch_size,
+                          shuffle=True,
+                          num_workers=0)
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 model = NeuralNet(input_size, hidden_size, output_size).to(device)
 
-# loss and optimizer 
+# Loss and optimizer
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr =learning_rate)
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
+# Train the model
 for epoch in range(num_epochs):
     for (words, labels) in train_loader:
         words = words.to(device)
@@ -99,20 +111,19 @@ for epoch in range(num_epochs):
     if (epoch+1) % 100 == 0:
         print (f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
 
+
 print(f'final loss: {loss.item():.4f}')
 
-# save / load model and implement the chat 
-
 data = {
-    "model_state" :model.state_dict(),
-    "input_size"  :input_size,
-    "output_size" :output_size,
-    "hidden_size" :hidden_size,
-    "all_words"   :all_words,
-    "tags"        :tags
+"model_state": model.state_dict(),
+"input_size": input_size,
+"hidden_size": hidden_size,
+"output_size": output_size,
+"all_words": all_words,
+"tags": tags
 }
 
 FILE = "data.pth"
-torch.save(data,FILE)
+torch.save(data, FILE)
 
 print(f'training complete. file saved to {FILE}')
